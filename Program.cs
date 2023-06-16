c4da21a95fb75ca703ac7cd199b8bea0f5bb5625
Irina <Irinakilina@icloud.com>
2023-06-16 18:28:42 +0300

    HW

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача №2
+
+/*Console.WriteLine("input first number: ");
+int numb1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter second number: ");
+int numb2 = Convert.ToInt32(Console.ReadLine());
+
+if (numb1 < numb2)
+{
+    Console.WriteLine("max number is: " + numb2);
+}
+else
+{
+    Console.WriteLine("min number is: " + numb1);
+}
+*/
+
+//Задача 4.
+
+/*Console.WriteLine("input first number: ");
+int numb1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("input second number: ");
+int numb2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("input third number: ");
+int numb3 = Convert.ToInt32(Console.ReadLine());
+
+
+if (numb1 < numb2);
+{
+    Console.WriteLine("max number is: " + numb2);
+}
+if (numb2 < numb3)
+{
+    Console.WriteLine("max number is: " + numb3);
+}
+*/
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+
+/*Console.WriteLine("input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
+*/
+
+//Задача 8: Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+
+/*Console.WriteLine("Enter your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+while (count <= number)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write(count + " ");
+        count += 1;
+    }
+}
+*/
